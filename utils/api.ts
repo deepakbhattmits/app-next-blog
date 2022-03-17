c85dcffd4ccf8f3de7217ba52e4c60a79a8e00ae
deepakbhattmits <deepakbhattmits@gmail.com>
2022-03-17 19:25:06 +0530

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: !!process.env.NODE_ENV?.match(/development/i)
     ? "http://localhost:3000"
-    : "LIVE UTL",
+    : "https://app-next-blog-zeta.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
