bb51ad86154a2001464ea5bb17d2325c02d923e3
deepakbhattmits <deepakbhattmits@gmail.com>
2022-03-28 14:25:07 +0530

    Update posts.ts

--- pages/api/v1/posts.ts
+++ pages/api/v1/posts.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 import fs from "fs/promises";
-import { getPostData } from "../../../utils/api-utils";
 export const fetchFeaturedPosts = async () => {
   const postsFilePath = path.join(process.cwd(), "db", "posts.json");
   const postsJsonData = await fs.readFile(postsFilePath);
@@ -19,11 +18,13 @@ export const fetchAllPosts = async () => {
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (!!req?.method?.match(/post/i)) {
-    //POST
-  }
   if (!!req?.method?.match(/get/i)) {
     const posts = await fetchAllPosts();
+    if (!!posts) {
+      res.status(200).send({ message: "Fetched successfully", posts });
+    } else {
+      res.status(422).send({ message: "Not found posts " });
+    }
   }
 };
 export default handler;
