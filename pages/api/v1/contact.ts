ba4da5df07255763512442f6faf31a39af315a9d
deepakbhattmits <deepakbhattmits@gmail.com>
2022-03-28 14:25:01 +0530

    Update contact.ts

--- pages/api/v1/contact.ts
+++ pages/api/v1/contact.ts
@@ -1,17 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-import fs from "fs/promises";
+import { connectToDatabase } from "../../../helpers";
 import { isValidEmail } from "../../../utils";
-export const fetchContacts = async () => {
-  const postsFilePath = path.join(process.cwd(), "db", "contacts.json");
-  const postsJsonData = await fs.readFile(postsFilePath);
-  const { data } = JSON.parse(postsJsonData.toString());
-
-  return data;
-};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  //POST
   if (!!req?.method?.match(/post/i)) {
     const { email, name, message } = req?.body;
 
@@ -28,22 +19,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         .json({ message: "Something wrong with inputs, please try again" });
       return;
     }
-    const newMessage = { email, name, message };
 
-    // write email to whole file
-    const contacts = await fetchContacts();
-    const contactFilePath = path.join(process.cwd(), "db", "contacts.json");
-    const postContact: { email: string }[] = [...contacts, newMessage];
-    const postContactJson = JSON.stringify({
-      status: "Request Completed",
-      statusCode: 200,
-      data: postContact,
+    const client = await connectToDatabase();
+    const db = client.db();
+    try {
+      await db.collection("conacts").insertOne({
+        email,
+        name,
+        message,
+      });
+    } catch (err) {
+      // console.error("If some error : ", err);
+    }
+    res.status(201).json({
+      message: "Data stored successfully",
+      data: { email, name, message },
     });
-
-    fs.writeFile(contactFilePath, postContactJson);
-    res
-      .status(201)
-      .json({ message: "Data stored successfully", data: newMessage });
+  } else {
+    res.status(200).send({ messgae: "request reached" });
   }
 };
 export default handler;
