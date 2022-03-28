275ecc6a3c27a0c4ca92bf5f88a76fbec57b7cfb
deepakbhattmits <deepakbhattmits@gmail.com>
2022-03-28 14:25:12 +0530

    Update api-utils.ts

--- utils/api-utils.ts
+++ utils/api-utils.ts
@@ -1,8 +1,8 @@
-import fs from 'fs/promises';
-import path from 'path';
-const postsDirectory = path.join(process.cwd(), 'posts');
-export const getPostData=(postIdentifier: string) =>{
+import fs from "fs/promises";
+import path from "path";
+const postsDirectory = path.join(process.cwd(), "posts");
+export const getPostData = (postIdentifier: string) => {
   const filePath = path.join(postsDirectory, `${postIdentifier}.md`);
-  const postContent=fs.readFile(filePath,'utf-8');
+  const postContent = fs.readFile(filePath, "utf-8");
   return postContent;
-}
+};
